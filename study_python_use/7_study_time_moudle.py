1f4811b0ef3026bbcc0f74f52275c698ea2f4675
Mr.Chen <820971427@qq.com>
2023-03-30 00:32:42 +0800

    新建处理时间模块文件脚本 2023-3-30

---
+++ study_python_use/7_study_time_moudle.py
@@ -0,0 +1,18 @@
+import time
+import threading
+import subprocess
+
+
+
+def takeANap():
+    time.sleep(5)
+    print('Wake up!')
+
+
+print('Start of program.')
+subprocess.Popen(r'"D:\Program Files (x86)\HuyaClient\HuyaClient\huya.exe"')
+subprocess.Popen(r'"C:\Program Files (x86)\Tencent\QQ\Bin\QQScLauncher.exe"')
+
+threadObj = threading.Thread(target=takeANap)
+threadObj.start()
+print('End of program.')
