d9684b049832bc723fcd0aec5844d5cdd22c79e8
Mr.Chen <820971427@qq.com>
2023-03-30 00:31:16 +0800

    新建处理文件脚本 2023-3-30

---
+++ study_python_use/3_deal_file.py
@@ -0,0 +1,23 @@
+import os
+
+
+current_path = os.getcwd()
+print(current_path)
+with open('../readme.txt') as tmp:
+    # for contents in tmp:
+    # contents = tmp.read()
+    #     print(contents, end='')
+        # 去除尾部空格
+    #     print(contents.rstrip())
+    lines = tmp.readlines()
+    print(lines)
+    words = ""
+    for line in lines:
+        words += line
+    print(words)
+    # 字符串 分割成列表
+    word = words.split()
+    print(word)
+    number_words = len(word)
+    print(number_words)
+
