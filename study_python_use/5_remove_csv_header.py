717d9e1cdb959d954e66a1059c4ef0d762deef01
Mr.Chen <820971427@qq.com>
2023-03-30 00:32:16 +0800

    新建处理csv-2文件脚本 2023-3-30

---
+++ study_python_use/5_remove_csv_header.py
@@ -0,0 +1,30 @@
+import os
+import csv
+
+csvRows = []
+# 找到所有的csv目标文件
+os.makedirs('../headerRemoved', exist_ok=True)
+for csvFilename in os.listdir('../tmp_files'):
+    if not csvFilename.endswith('.csv'):
+        continue
+    print(csvFilename)
+    # 读入csv文件
+    csvFile = open('../tmp_files/%s' % csvFilename)
+    readerCsv = csv.reader(csvFile)
+    for row in readerCsv:
+        if readerCsv.line_num == 1:
+            continue
+        csvRows.append(row)
+    csvFile.close()
+print(csvRows)
+# 写入csv文件，丢弃第一行
+csvFileObj = open(os.path.join('../headerRemoved', 'finish_file.csv'), 'w', newline='')
+csvWriter = csv.writer(csvFileObj)
+for row in csvRows:
+    csvWriter.writerow(row)
+csvFileObj.close()
+
+
+
+
+
